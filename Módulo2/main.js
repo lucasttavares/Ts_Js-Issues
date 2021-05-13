a4fa5f99597a1131fa59b6dc63872afe36e165a2
ldavi8533 <75754211+ldavi8533@users.noreply.github.com>
2021-05-13 13:01:27 -0300

    Add files via upload

---
+++ Módulo2/main.js
@@ -0,0 +1,8 @@
+import Usuario from './functions';
+
+Usuario.info();
+
+import { idade } from './functions';
+console.log(idade);
+
+import defaultUsuario, { idade as IdadeUsuario } from './functions';
